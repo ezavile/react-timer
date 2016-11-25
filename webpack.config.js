d48063cc7c500bea858c162fa4d6479214b7ed02
ezavile <ezavile@gmail.com>
2016-11-24 23:51:24 -0600

    adding static controls component

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
       Countdown: 'app/components/Countdown.jsx',
       Timer: 'app/components/Timer.jsx',
       CountdownForm: 'app/components/CountdownForm.jsx',
-      Clock: 'app/components/Clock.jsx'
+      Clock: 'app/components/Clock.jsx',
+      Controls: 'app/components/Controls.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
